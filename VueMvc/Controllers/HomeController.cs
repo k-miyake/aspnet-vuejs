451e7189438ebc908f96cacb8253c8d3310674fe
Kazuyuki Miyake <miyake@zenarc.jp>
2017-12-19 21:16:42 +0900

    add Todo page

--- VueMvc/Controllers/HomeController.cs
+++ VueMvc/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace VueMvc.Controllers
             return View();
         }
 
+        public ActionResult IndexVue()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ VueMvc/Scripts/todo.vue.js
@@ -0,0 +1,15 @@
+﻿var app = new Vue({
+    el: "#todo-list",
+    data: {
+        todoList: [
+            {
+                summary: "1つ目のtodo",
+                done: true
+            },
+            {
+                summary: "2つ目のtodo",
+                done: false
+            }
+        ]
+    }
+});
